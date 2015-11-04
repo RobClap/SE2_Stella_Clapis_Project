e56b3e3b5d78a0980304fae9915743df82eda1c8
RobClap <robclap8@gmail.com>
2015-11-04 14:13:01 +0100

    Alloy works! Ye!

--- RASD/Alloy/converter.sh
+++ RASD/Alloy/converter.sh
@@ -6,6 +6,7 @@ sed -r \
 	-e 's:\{:\\\{:g' \
 	-e '/^\//! s:\s(abstract|and|as|assert|but|check|else|exactly|enum|fact|for|fun|module|iff|implies|Int|let|in|open|or|pred|run|sum|extends|univ|iden|one|lone|all|some|no|none|disj|not|set|sig)\s: {\\color{blue}\1} :g' \
 	-e 's:\^:\\string\^:g' \
+	-e 's:\#:\\\#:g' \
 	-e 's:(//.*):{\\color{green}\1}:g' \
 	-e 's:(/\*.*):{\\color{green}\1:g' \
 	-e 's:(.*\*/):\1}:g' \
